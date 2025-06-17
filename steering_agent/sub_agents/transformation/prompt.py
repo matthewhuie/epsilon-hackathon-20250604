b70641288f3b0990c10cb257e53b30ee021a2033
Matthew Huie <matthew@matthewhuie.com>
2025-06-17 12:39:54 -0400

    Further refinement

--- steering_agent/sub_agents/transformation/prompt.py
+++ steering_agent/sub_agents/transformation/prompt.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TRANSFORMATION_AGENT_PROMPT = """Ignore other input data.  Given the below JSON array of column mappings, recommend transformations for each column mapping.
+TRANSFORMATION_AGENT_PROMPT = """Given the below JSON array of column mappings, use recommend_transformations tool for each column mapping.
     
     **JSON array of column mappings:**
     {classifier_agent_response}
